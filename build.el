a3c796fe6a0e9c1136a44946be83e8fd1ac97c7b
Bastian Mathison <bastian.mathison@education.nsw.gov.au>
2022-08-08 15:06:20 +1000

    finished special relativity

--- build.el
+++ build.el
@@ -12,19 +12,20 @@
              ;; disabling
              :with-toc nil
              :with-creator nil
-             :section-numbers nil
+             :section-numbers nil)))
 
-             ;; sitemap
-             :auto-sitemap t
-             :sitemap-file "common/sitemap.org"
-             :sitemap-title "Sitemap"
-             )))
-
+;; sets HTML specific variables
 (setq org-html-validation-link nil)
 (setq org-html-footnote-format nil)
 (setq org-html-postamble nil)
 (setq org-html-head "<link rel=\"stylesheet\" href=\"https://cdn.simplecss.org/simple.min.css\">")
 
+;; publishes all
 (org-publish-all t)
 
+(if (file-directory-p "public/images")
+    (delete-directory "public/images" t))
+(copy-directory "content/images" "public/")
+
+;; helpful message
 (message "Build complete!")
